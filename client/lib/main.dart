d39ab8ee52ab3cae60520cf8785f1e409340880d
Manuel Canas <canasmh@yahoo.com>
2024-11-23 01:07:45 -0500

    Refactor main.dart
    Lock in screen orientation and establish a SafeArea

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,12 +1,30 @@
-// Start by cleaning up main.dart to just this:
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:iron_track/core/theme/app_theme.dart';
 import 'package:iron_track/features/auth/screens/welcome_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Lock orientation to portrait
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Set system overlay style
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
+/// The root widget of the Iron Track application.
+/// Configures global application settings like theme and routing.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,7 +32,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Iron Track',
-      theme: AppTheme.themeData,
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
       home: const WelcomeScreen(),  // We'll create this next
     );
   }

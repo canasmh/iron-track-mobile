c5653d23915f48c2380c55b79aa171ccebbbbd5c
Manuel Canas <canasmh@yahoo.com>
2024-11-24 22:26:00 -0500

    Update social_login_section test suite
    I guess there was some formatting change or something

--- client/test/features/auth/widgets/social_login_section_test.dart
+++ client/test/features/auth/widgets/social_login_section_test.dart
@@ -32,25 +32,26 @@ void main() {
       );
     }
 
-    testWidgets('renders divider with correct text', (WidgetTester tester) async {
+    testWidgets('renders divider with correct text',
+        (WidgetTester tester) async {
       await tester.pumpWidget(buildTestWidget());
 
       // Verify divider text
       expect(
-        find.text(AuthStrings.social.continueWith), 
+        find.text(AuthStrings.social.continueWith),
         findsOneWidget,
         reason: 'Should show "Continue With" text',
       );
 
       // Verify dividers
       expect(
-        find.byType(Divider), 
+        find.byType(Divider),
         findsNWidgets(2),
         reason: 'Should show two dividers',
       );
     });
 
-    testWidgets('renders all social buttons with correct icons', 
+    testWidgets('renders all social buttons with correct icons',
         (WidgetTester tester) async {
       await tester.pumpWidget(buildTestWidget());
 
@@ -59,41 +60,33 @@ void main() {
 
       // Verify specific icons
       expect(
-        find.byWidgetPredicate(
-          (widget) => widget is FaIcon && 
-                     widget.icon == FontAwesomeIcons.facebook
-        ), 
+        find.byWidgetPredicate((widget) =>
+            widget is FaIcon && widget.icon == FontAwesomeIcons.facebook),
         findsOneWidget,
       );
       expect(
-        find.byWidgetPredicate(
-          (widget) => widget is FaIcon && 
-                     widget.icon == FontAwesomeIcons.xTwitter
-        ), 
+        find.byWidgetPredicate((widget) =>
+            widget is FaIcon && widget.icon == FontAwesomeIcons.xTwitter),
         findsOneWidget,
       );
       expect(
-        find.byWidgetPredicate(
-          (widget) => widget is FaIcon && 
-                     widget.icon == FontAwesomeIcons.google
-        ), 
+        find.byWidgetPredicate((widget) =>
+            widget is FaIcon && widget.icon == FontAwesomeIcons.google),
         findsOneWidget,
       );
       expect(
-        find.byWidgetPredicate(
-          (widget) => widget is FaIcon && 
-                     widget.icon == FontAwesomeIcons.apple
-        ), 
+        find.byWidgetPredicate((widget) =>
+            widget is FaIcon && widget.icon == FontAwesomeIcons.apple),
         findsOneWidget,
       );
     });
 
-    testWidgets('social buttons have correct tooltips', 
+    testWidgets('social buttons have correct tooltips',
         (WidgetTester tester) async {
       await tester.pumpWidget(buildTestWidget());
 
       final buttons = tester.widgetList<IconButton>(find.byType(IconButton));
-      
+
       expect(
         buttons.any((button) => button.tooltip == 'Continue with Facebook'),
         isTrue,
@@ -112,7 +105,7 @@ void main() {
       );
     });
 
-    testWidgets('buttons trigger correct callbacks when pressed', 
+    testWidgets('buttons trigger correct callbacks when pressed',
         (WidgetTester tester) async {
       // ARRANGE
       bool facebookPressed = false;
@@ -130,38 +123,30 @@ void main() {
       );
 
       // ACT & ASSERT - Facebook
-      await tester.tap(find.byWidgetPredicate(
-        (widget) => widget is FaIcon && 
-                    widget.icon == FontAwesomeIcons.facebook
-      ));
+      await tester.tap(find.byWidgetPredicate((widget) =>
+          widget is FaIcon && widget.icon == FontAwesomeIcons.facebook));
       expect(facebookPressed, isTrue);
       expect(twitterPressed, isFalse);
       expect(googlePressed, isFalse);
       expect(applePressed, isFalse);
 
       // ACT & ASSERT - Twitter
-      await tester.tap(find.byWidgetPredicate(
-        (widget) => widget is FaIcon && 
-                    widget.icon == FontAwesomeIcons.xTwitter
-      ));
+      await tester.tap(find.byWidgetPredicate((widget) =>
+          widget is FaIcon && widget.icon == FontAwesomeIcons.xTwitter));
       expect(twitterPressed, isTrue);
 
       // ACT & ASSERT - Google
-      await tester.tap(find.byWidgetPredicate(
-        (widget) => widget is FaIcon && 
-                    widget.icon == FontAwesomeIcons.google
-      ));
+      await tester.tap(find.byWidgetPredicate((widget) =>
+          widget is FaIcon && widget.icon == FontAwesomeIcons.google));
       expect(googlePressed, isTrue);
 
       // ACT & ASSERT - Apple
-      await tester.tap(find.byWidgetPredicate(
-        (widget) => widget is FaIcon && 
-                    widget.icon == FontAwesomeIcons.apple
-      ));
+      await tester.tap(find.byWidgetPredicate((widget) =>
+          widget is FaIcon && widget.icon == FontAwesomeIcons.apple));
       expect(applePressed, isTrue);
     });
 
-    testWidgets('buttons are disabled when callbacks are null', 
+    testWidgets('buttons are disabled when callbacks are null',
         (WidgetTester tester) async {
       await tester.pumpWidget(buildTestWidget());
 
@@ -171,17 +156,19 @@ void main() {
       }
     });
 
-    testWidgets('maintains correct spacing between buttons', 
+    testWidgets('maintains correct spacing between buttons',
         (WidgetTester tester) async {
       await tester.pumpWidget(buildTestWidget());
 
-      final spacers = tester.widgetList<SizedBox>(
-        find.byType(SizedBox),
-      ).where((spacer) => spacer.width != null);
+      final spacers = tester
+          .widgetList<SizedBox>(
+            find.byType(SizedBox),
+          )
+          .where((spacer) => spacer.width != null);
 
       for (final spacer in spacers) {
         expect(
-          spacer.width, 
+          spacer.width,
           16.0,
           reason: 'Horizontal spacing between buttons should be 16.0',
         );
